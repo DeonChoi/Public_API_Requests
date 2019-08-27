a3e5c5ebe9a0d3b11fb4e69df27583f72698bd10
Deon Choi <deon.choi@gmail.com>
2019-08-27 01:33:55 -0700

    Third Update
    When the modal window is brought up, you are able to use the 'next' and 'previous' buttons to cycle through the directory. I made the user able to infinitely loop through the directory, forwards and backwards, without error.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,7 @@
 fetch('https://randomuser.me/api/?results=12')
     .then(response => response.json())
     .then(data => generateUsers(data.results))
-    //.then(users => console.log(JSON.stringify(users)))
     .catch(err => console.log('Fetch Error :-S', err));
-
 
 //method to use the received data from the API and generate employee 'cards' onto the webpage
 function generateUsers(allUsers) {
@@ -23,75 +21,176 @@ function generateUsers(allUsers) {
                 </div>
             </div>
         `;
-
         $('#gallery').append(newUserCard);
     })
 
-    //click event handler for all employee 'cards' on the page
-    $('.card').on('click', function() {
-        console.log($(this)[0].children[0].childNodes[1].attributes[1].nodeValue);
+    employeeClick();
+    //function for click event handler for all employee 'cards' on the page
+    function employeeClick() {
+        $('.card').on('click', function() {
+            //assigns the image url of the clicked employee 'card' to a constant named 'clickedUserSRC'
+            const clickedUserSRC = $(this)[0].children[0].childNodes[1].attributes[1].nodeValue;
+            //initializes variable 'clickedUser' which is null, but will store the clicked user object
+            let clickedUser = null;
 
-        //assigns the image url of the clicked employee 'card' to a constant named 'clickedUserSRC'
-        const clickedUserSRC = $(this)[0].children[0].childNodes[1].attributes[1].nodeValue;
-        //initializes variable 'clickedUser' which is null, but will store the clicked user object
-        let clickedUser = null;
+            //iterates through user directory checking for a user with matching image url as 'clickedUserSRC'
+            //it then stores that matched user to variable 'clickedUser'
+            // allUsers.forEach(user => {
+            //     if (clickedUserSRC === user.picture.medium) {
+            //         clickedUser = user;
+            //         console.log(clickedUser);
+            //         console.log(allUsers);
+            //     }
+            // })
 
-        //iterates through user directory checking for a user with matching image url as 'clickedUserSRC'
-        //it then stores that matched user to variable 'clickedUser'
-        allUsers.forEach(user => {
-            if (clickedUserSRC === user.picture.medium) {
-                //console.log('yes!')
-                clickedUser = user;
-                console.log(clickedUser);
-            } else {
-                //console.log('no!')
+            for (let i = 0; i < allUsers.length; i++) {
+                if (clickedUserSRC === allUsers[i].picture.medium) {
+                    clickedUser = allUsers[i];
+                    console.log(clickedUser);
+                }
             }
-        })
 
-        //converts user birthday for mm/dd/yyyy format
-        let clickedUserDOB = '';
-        getBirthday(clickedUser);
+            //index of clicked user in the array of users
+            let indexOfClickedUser = allUsers.indexOf(clickedUser);
 
-        function getBirthday(user) {
-            clickedUserDOB = new Date(user.dob.date);
-            var dd = clickedUserDOB.getDate();
-            var mm = clickedUserDOB.getMonth() + 1;
-            var yyyy = clickedUserDOB.getFullYear();
-            if (dd < 10) {
-                dd = '0' + dd;
+            //converts user birthday for mm/dd/yyyy format
+            let clickedUserDOB = '';
+            getBirthday(clickedUser);
+
+            //converts user birthday for mm/dd/yyyy format
+            function getBirthday(user) {
+                clickedUserDOB = new Date(user.dob.date);
+                var dd = clickedUserDOB.getDate();
+                var mm = clickedUserDOB.getMonth() + 1;
+                var yyyy = clickedUserDOB.getFullYear();
+                if (dd < 10) {
+                    dd = '0' + dd;
+                }
+                if (mm < 10) {
+                    mm = '0' + mm;
+                }
+                clickedUserDOB = mm + '/' + dd + '/' + yyyy;
             }
-            if (mm < 10) {
-                mm = '0' + mm;
-            }
-            clickedUserDOB = mm + '/' + dd + '/' + yyyy;
-        }
 
-        //template literal for clicked user modal window that will be appended to the webpage
-        const clickedUserWindow = `
-            <div class="modal-container">
-                <div class="modal">
-                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                    <div class="modal-info-container">
-                        <img class="modal-img" src=${clickedUser.picture.medium} alt="profile picture">
-                        <h3 id="name" class="modal-name cap">${clickedUser.name.first} ${clickedUser.name.last}</h3>
-                        <p class="modal-text">${clickedUser.email}</p>
-                        <p class="modal-text cap">${clickedUser.location.city}, ${clickedUser.location.state}</p>
-                        <hr>
-                        <p class="modal-text">${clickedUser.cell}</p>
-                        <p class="modal-text" id="modal-location">${clickedUser.location.street}, ${clickedUser.location.city.charAt(0).toUpperCase() + clickedUser.location.city.slice(1)}, ${clickedUser.location.state.charAt(0).toUpperCase() + clickedUser.location.state.slice(1)} ${clickedUser.location.postcode}</p>
-                        <p class="modal-text">Birthday: ${clickedUserDOB}</p>
+
+            function appendModalWindow(clickedUser) {
+                //template literal for clicked user modal window that will be appended to the webpage
+                const clickedUserWindow = `
+                <div class="modal-container">
+                    <div class="modal">
+                        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                        <div class="modal-info-container">
+                            <img class="modal-img" src=${clickedUser.picture.medium} alt="profile picture">
+                            <h3 id="name" class="modal-name cap">${clickedUser.name.first} ${clickedUser.name.last}</h3>
+                            <p class="modal-text">${clickedUser.email}</p>
+                            <p class="modal-text cap">${clickedUser.location.city}, ${clickedUser.location.state}</p>
+                            <hr>
+                            <p class="modal-text">${clickedUser.cell}</p>
+                            <p class="modal-text" id="modal-location">${clickedUser.location.street}, ${clickedUser.location.city.charAt(0).toUpperCase() + clickedUser.location.city.slice(1)}, ${clickedUser.location.state.charAt(0).toUpperCase() + clickedUser.location.state.slice(1)} ${clickedUser.location.postcode}</p>
+                            <p class="modal-text">Birthday: ${clickedUserDOB}</p>
+                        </div>
+                        <div class="modal-btn-container">
+                            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        `;
+            `;
+                $('body').append(clickedUserWindow);
+                $('#modal-next').on('click', function() {
+                    resetNextUser();
+                });
+                $('#modal-prev').on('click', function() {
+                    resetPrevUser();
+                });
+            }
 
-        //$('.modal-location').css('text-transform', 'capitalize');
-        $('body').append(clickedUserWindow);
+            appendModalWindow(clickedUser);
+            closeModalWindow();
+            //$('.modal-location').css('text-transform', 'capitalize');
 
-        //click event handler to close modal window when you click the 'X' button
-        $('.modal-close-btn').on('click', function() {
-            $('.modal-container').remove();
-        });
+            //next user is initalized by increasing index of current user by 1
+            //previous user is initialized by decreasing index of current user by 1
+            let nextUser = allUsers[indexOfClickedUser + 1];
+            let prevUser = allUsers[indexOfClickedUser - 1];
 
-    })
+            //sets next user to current user
+            // function resetNextUser() {
+            //     if (indexOfClickedUser <= 10) {
+            //         $('.modal-container').remove();
+            //         appendModalWindow(nextUser);
+            //         indexOfClickedUser += 1;
+            //         clickedUser = nextUser;
+            //         nextUser = allUsers[indexOfClickedUser + 1];
+            //         prevUser = allUsers[indexOfClickedUser - 1];
+            //         closeModalWindow();
+            //     } else {
+            //         alert("You have reached the end of the directory");
+            //     }
+            // }
+            function resetNextUser() {
+                if (indexOfClickedUser < 10) {
+                    $('.modal-container').remove();
+                    appendModalWindow(nextUser);
+                    indexOfClickedUser += 1;
+                    clickedUser = nextUser;
+                    nextUser = allUsers[indexOfClickedUser + 1];
+                    prevUser = allUsers[indexOfClickedUser - 1];
+                    closeModalWindow();
+                } else if (indexOfClickedUser = 11) {
+                    $('.modal-container').remove();
+                    indexOfClickedUser = -1;
+                    appendModalWindow(nextUser);
+                    clickedUser = nextUser;
+                    nextUser = allUsers[0];
+                    prevUser = allUsers[indexOfClickedUser - 1];
+                    closeModalWindow();
+                }
+            }
+
+            //sets previous user to current user
+            function resetPrevUser() {
+                if (indexOfClickedUser > 1) {
+                    $('.modal-container').remove();
+                    appendModalWindow(prevUser);
+                    indexOfClickedUser -= 1;
+                    clickedUser = prevUser;
+                    nextUser = allUsers[indexOfClickedUser + 1];
+                    prevUser = allUsers[indexOfClickedUser - 1];
+                    closeModalWindow();
+                } else if (indexOfClickedUser = 1) {
+                    $('.modal-container').remove();
+                    indexOfClickedUser = 12;
+                    appendModalWindow(prevUser);
+                    clickedUser = prevUser;
+                    nextUser = allUsers[1];
+                    prevUser = allUsers[11];
+                    closeModalWindow();
+                }
+            }
+            // function resetPrevUser() {
+            //     if (indexOfClickedUser >= 1) {
+            //         $('.modal-container').remove();
+            //         appendModalWindow(prevUser);
+            //         indexOfClickedUser -= 1;
+            //         clickedUser = prevUser;
+            //         nextUser = allUsers[indexOfClickedUser + 1];
+            //         prevUser = allUsers[indexOfClickedUser - 1];
+            //         closeModalWindow();
+            //     } else {
+            //         alert("You have reached the beginning of the directory");
+            //     }
+            // }
+
+            //click event handler inside a function, used to close modal window when you click the 'X' button
+            function closeModalWindow() {
+                $('.modal-close-btn').on('click', function() {
+                    $('.modal-container').remove();
+                });
+            }
+
+        })
+
+    }
+
 }
